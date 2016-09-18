7a01507022d42b79d490b8d27e7a0da44e460f53
ZauberNerd <zaubernerd@zaubernerd.de>
2016-09-18 21:42:52 +0200

    feat(infrastructure): invoke lambda function every 5 minutes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,12 @@ resource "aws_iam_role" "iam_for_dependency_updater" {
     assume_role_policy = "${file("${path.module}/assumeRole.json")}"
 }
 
+resource "aws_iam_role_policy" "test_policy" {
+    name = "test_policy"
+    role = "${aws_iam_role.iam_for_dependency_updater.id}"
+    policy = "${file("${path.module}/rolePolicy.json")}"
+}
+
 resource "aws_lambda_function" "dependency_updater" {
     filename = "distribution/lambda.zip"
     function_name = "dependency-updater"
@@ -14,4 +20,23 @@ resource "aws_lambda_function" "dependency_updater" {
     handler = "distribution/index.handler"
     runtime = "nodejs4.3"
     source_code_hash = "${base64sha256(file("distribution/lambda.zip"))}"
+}
+
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "execute_dependency_updater_every_5_minutes" {
+    rule = "${aws_cloudwatch_event_rule.every_five_minutes.name}"
+    arn = "${aws_lambda_function.dependency_updater.arn}"
+}
+
+resource "aws_lambda_permission" "connect_cloudwatch_with_dependency_updater" {
+    statement_id = "dependencyUpdaterEvery5Minutes"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.dependency_updater.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.every_five_minutes.arn}"
 }
