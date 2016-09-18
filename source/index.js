4b1ec6a929162df390c34dd7d345c613b5b8006f
ZauberNerd <zaubernerd@zaubernerd.de>
2016-09-18 23:21:05 +0200

    docs: add source code comments for TODOs and execution flows

--- source/index.js
+++ source/index.js
@@ -9,6 +9,9 @@ exports.handler = function(event, context, callback) {
 async function updateDependencies() {
   const github = new GitHub();
 
+  // XXX: this is static at the moment, later it will of course be configurable
+  // TODO: do we want to check multiple file paths (mono-repo)?
+  // TODO: do we want to check multiple repositories per invocation?
   const result = await github.repos.getContent({
     user: 'nerdlabs',
     repo: 'react-amp-layout',
@@ -19,12 +22,22 @@ async function updateDependencies() {
   const fileContent = new Buffer(content, encoding).toString('utf-8');
   const data = JSON.parse(fileContent);
   const updatedDependencies = await getUpdatedDependencies(data);
+
+  // for every updated dependency:
+  // - check whether a PR for this update already exists: https://mikedeboer.github.io/node-github/#api-pullRequests-getAll
+  // - update version number in package.json
+  // - create a new branch: https://mikedeboer.github.io/node-github/#api-gitdata-createReference
+  // - update the package.json file in the newly created branch: https://mikedeboer.github.io/node-github/#api-repos-updateFile
+  // - create a new pull request: https://mikedeboer.github.io/node-github/#api-pullRequests-create
+
   return updatedDependencies;
 }
 
 // TODO: extract into its own file
 import RegistryClient from 'npm-registry-client';
+
 const noop = () => {};
+
 const config = {
   log: {
     error: noop,
@@ -37,6 +50,7 @@ const config = {
     resume: noop
   }
 };
+
 const client = new RegistryClient(config);
 
 function getPackage(name) {
@@ -51,6 +65,7 @@ function getPackage(name) {
 import semver from 'semver';
 
 async function getUpdatedDependencies(manifest) {
+  // XXX: are there any other *Dependencies that we should include?
   const {dependencies = {}, devDependencies = {}} = manifest;
   const allDependencies = {...dependencies, ...devDependencies};
 
@@ -60,11 +75,14 @@ async function getUpdatedDependencies(manifest) {
 
   const latestVersions = packages.map((manifest) => ({
     name: manifest.name,
+    // XXX: should we also check for other dist-tags?
     latest: manifest['dist-tags'].latest
   }));
 
   return latestVersions.filter(({name, latest}) => {
     const current = allDependencies[name];
+    // XXX: maybe also verify that this update would not already be covered by the
+    // current semver range to reduce the amount of unneccessary PRs
     return semver.gt(latest, current);
   });
 }
