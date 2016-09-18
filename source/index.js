a07b938902f694fb66da888f03327540357bcb43
ZauberNerd <zaubernerd@zaubernerd.de>
2016-09-18 21:59:04 +0200

    feat(updater): fetch dependency packages and compare versions
    fetch package metadata from dependencies and devDependencies and return
    a list of outdated dependencies and their latest stable version.

--- source/index.js
+++ source/index.js
@@ -17,6 +17,54 @@ async function updateDependencies() {
 
   const {encoding = 'base64', content} = result;
   const fileContent = new Buffer(content, encoding).toString('utf-8');
+  const data = JSON.parse(fileContent);
+  const updatedDependencies = await getUpdatedDependencies(data);
+  return updatedDependencies;
+}
 
-  return JSON.parse(fileContent);
+// TODO: extract into its own file
+import RegistryClient from 'npm-registry-client';
+const noop = () => {};
+const config = {
+  log: {
+    error: noop,
+    warn: noop,
+    info: noop,
+    verbose: noop,
+    silly: noop,
+    http: noop,
+    pause: noop,
+    resume: noop
+  }
+};
+const client = new RegistryClient(config);
+
+function getPackage(name) {
+  return new Promise((resolve, reject) => {
+    const uri = `https://registry.npmjs.org/${name}`;
+
+    client.get(uri, {}, (error, data) => error ? reject(error) : resolve(data));
+  });
+}
+
+// TODO: extract into its own file
+import semver from 'semver';
+
+async function getUpdatedDependencies(manifest) {
+  const {dependencies = {}, devDependencies = {}} = manifest;
+  const allDependencies = {...dependencies, ...devDependencies};
+
+  const packages = await Promise.all(
+    Object.keys(allDependencies).map(getPackage)
+  );
+
+  const latestVersions = packages.map((manifest) => ({
+    name: manifest.name,
+    latest: manifest['dist-tags'].latest
+  }));
+
+  return latestVersions.filter(({name, latest}) => {
+    const current = allDependencies[name];
+    return semver.gt(latest, current);
+  });
 }
