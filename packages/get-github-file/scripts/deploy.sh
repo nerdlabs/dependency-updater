c7cf3555b1dc4f3fbb58e67434d1887e0f58973b
ZauberNerd <zaubernerd@zaubernerd.de>
2016-10-30 19:38:24 +0100

    fix(deploy): push commits & tags made by npm to github

--- packages/get-github-file/scripts/deploy.sh
+++ packages/get-github-file/scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
 npm version prerelease
+git push && git push --tags
 npm publish --tag=canary
