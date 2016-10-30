fe75f6721fccbf8942bda2861d2834727405ec17
ZauberNerd <zaubernerd@zaubernerd.de>
2016-10-30 20:05:47 +0100

    fix(deploy): use travis-after-all to publish only once

--- packages/get-github-file/scripts/deploy.sh
+++ packages/get-github-file/scripts/deploy.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-git config --global user.email "builds@travis-ci.com"
-git config --global user.name "Travis CI"
-npm version prerelease
-git push && git push --tags
-npm publish --tag=canary
+declare exitCode;
+
+
+# -- [1] -------------------------------------------------------
+
+$(npm bin)/travis-after-all
+exitCode=$?
+
+
+# -- [2] -------------------------------------------------------
+
+if [ $exitCode -eq 0 ]; then
+  git config --global user.email "builds@travis-ci.com"
+	git config --global user.name "Travis CI"
+	npm version prerelease
+	git push && git push --tags
+	npm publish --tag=canary
+fi
+
+if [ $exitCode -eq 1 ]; then
+  echo "One or more travis jobs for this build failed."
+fi
