c092d5e66dfa0178e809f1238fc81efbe5ed3bd2
ZauberNerd <zaubernerd@zaubernerd.de>
2017-10-03 17:41:43 +0200

    refactor(core): working prototype
    features:
    - amends previously opened PRs
    - picks up state from previously opened PRs
    - ignores unpublished packages (prints a warning)
    - works with monorepos

--- @dependency-updater/core/source/index.js
+++ @dependency-updater/core/source/index.js
@@ -1,13 +1,12 @@
+import { basename, dirname, relative, sep as pathSeparator } from 'path';
 import unindent from 'unindent';
 import getGithubFile from '@dependency-updater/get-file';
-import createGithubPullRequest from '@dependency-updater/create-pr';
 import semver from 'semver';
 import latestVersion from 'latest-version';
 import Github from 'github';
 import parseJson from 'parse-json';
-import getManifests from './get-manifests';
 
-exports.handler = function(event, context, callback) {
+exports.handler = function handler(event, context, callback) {
     main({
         auth: {
             type: 'basic',
@@ -21,86 +20,231 @@ exports.handler = function(event, context, callback) {
         .catch(callback);
 };
 
-async function main({ auth, user, repo }) {
-    const client = new Github();
-    client.authenticate(auth);
-    const manifests = await Promise.all(
-        (await getManifests(client, user, repo))
-            .map(({ path }) => {
-                const file = getGithubFile(`${user}/${repo}/${path}`, {
-                    client,
-                });
-                return { path, file };
-            })
-            .map(async ({ path, file }) => {
-                const content = parseJson((await file).toString('utf-8'));
-                return { path, content };
-            }),
-    );
+function ignoreNodeModules({ path }) {
+    return !path.includes('node_modules');
+}
 
-    await Promise.all(
-        manifests.map(async ({ path, content: manifest }) => {
-            const updates = await getUpdates(manifest);
-            if (
-                !updates.dependencies.length &&
-                !updates.devDependencies.length
-            ) {
-                return;
-            }
-            const newManifest = await updateDependencies(manifest, updates);
-            const message = 'Update dependencies';
-            console.log(newManifest);
-            // return createGithubPullRequest(
-            // 	`${user}/${repo}/${path}`,
-            // 	new Buffer(newManifest),
-            // 	{ client, message }
-            // );
-        }),
+function matchFile(fileName, { type, path }) {
+    return type === 'blob' && basename(path) === fileName;
+}
+
+function matchManifest(item) {
+    return matchFile('package.json', item);
+}
+
+function matchConfigFile(item) {
+    return matchFile('.npmrc', item) || matchFile('.yarnrc', item);
+}
+
+function matchLockFile(item) {
+    return (
+        matchFile('yarn.lock', item) ||
+        matchFile('package-lock.json', item) ||
+        matchFile('npm-shrinkwrap.json', item)
     );
 }
 
-async function getUpdates(manifest) {
-    const { dependencies = {}, devDependencies = {} } = manifest;
-    const latestDependencies = await getLatestVersions(dependencies);
-    const latestDevDependencies = await getLatestVersions(devDependencies);
-    return {
-        dependencies: latestDependencies,
-        devDependencies: latestDevDependencies,
+function matchOnlyParents(cwd) {
+    return function(path) {
+        const dir = dirname(path);
+        return relative(cwd, dir)
+            .split(pathSeparator)
+            .every(x => x === '..' || x === '');
     };
 }
 
-async function updateDependencies(manifest, { dependencies, devDependencies }) {
-    dependencies.forEach(({ name, latest }) => {
-        manifest.dependencies[name] = latest;
-    });
-    devDependencies.forEach(({ name, latest }) => {
-        manifest.devDependencies[name] = latest;
-    });
-
-    return JSON.stringify(manifest, null, 2);
+function getBranchName(path) {
+    return `update-dependencies-${path === '.' ? 'root' : path}`;
 }
 
 async function getLatestVersions(dependencies) {
     return (await Promise.all(
-        Object.keys(dependencies).map(async name => ({
-            name,
-            current: dependencies[name],
-            latest: await latestVersion(name),
-        })),
-    ))
-        .filter(({ name, current, latest }) => {
-            return (
-                Boolean(semver.validRange(current)) &&
-                !semver.satisfies(latest, current)
-            );
-        })
-        .map(dependency => {
-            if (dependency.current.startsWith('~')) {
-                dependency.latest = `~${dependency.latest}`;
+        Object.keys(dependencies).map(async name => {
+            try {
+                const current = dependencies[name];
+                const latest = await latestVersion(name);
+                if (
+                    Boolean(semver.validRange(current)) &&
+                    !semver.satisfies(latest, current)
+                ) {
+                    let currentRange = '';
+                    if (current.startsWith('~')) {
+                        currentRange = '~';
+                    }
+                    if (current.startsWith('^')) {
+                        currentRange = '^';
+                    }
+                    return {
+                        name,
+                        current,
+                        latest: `${currentRange}${latest}`,
+                    };
+                }
+            } catch (error) {
+                console.warn(error);
             }
-            if (dependency.current.startsWith('^')) {
-                dependency.latest = `^${dependency.latest}`;
-            }
-            return dependency;
+            return null;
+        }),
+    )).filter(Boolean);
+}
+
+async function getUpdates(manifest) {
+    const { dependencies = {}, devDependencies = {} } = manifest;
+    const [latestDependencies, latestDevDependencies] = await Promise.all([
+        getLatestVersions(dependencies),
+        getLatestVersions(devDependencies),
+    ]);
+    return [
+        ...latestDependencies.map(x => ((x.type = 'dependencies'), x)),
+        ...latestDevDependencies.map(x => ((x.type = 'devDependencies'), x)),
+    ];
+}
+
+async function main({ auth, user, repo }) {
+    const client = new Github();
+    client.authenticate(auth);
+
+    const [
+        { default_branch: defaultBranch },
+        openPRs,
+        { tree: rawTree },
+    ] = await Promise.all([
+        client.repos.get({ user, repo }),
+        client.pullRequests.getAll({ user, repo, state: 'open' }),
+        client.gitdata.getTree({ user, repo, sha: 'HEAD', recursive: true }),
+    ]);
+
+    const ourPRs = openPRs.filter(({ user }) => user.login === auth.username);
+    const tree = rawTree.filter(ignoreNodeModules);
+    const manifests = tree.filter(matchManifest).map(item => item.path);
+    const configFiles = tree.filter(matchConfigFile).map(item => item.path);
+    const lockFiles = tree.filter(matchLockFile).map(item => item.path);
+
+    function getPackageConfig(path) {
+        const dir = dirname(path);
+        return {
+            path: dir,
+            manifestPath: path,
+            configFiles: configFiles.filter(matchOnlyParents(dir)),
+            lockFiles: lockFiles.filter(matchOnlyParents(dir)),
+        };
+    }
+
+    function setBranchConfig(packageConfig) {
+        const branchName = getBranchName(packageConfig.path);
+        const headBranch = ourPRs.find(pr => pr.head.ref === branchName);
+
+        return Object.assign({}, packageConfig, {
+            baseBranch: defaultBranch,
+            headBranch: headBranch ? headBranch.head.ref : null,
+            prExists: Boolean(headBranch),
         });
+    }
+
+    async function parsePackageFiles(packageConfig) {
+        const path = packageConfig.manifestPath;
+        const manifest = await getGithubFile(`${user}/${repo}/${path}`, {
+            client,
+            branch: packageConfig.headBranch || packageConfig.baseBranch,
+        });
+        return Object.assign({}, packageConfig, {
+            manifest: parseJson(manifest.toString('utf-8')),
+            // TODO: parse npm config
+            // TODO: parse yarn config
+        });
+    }
+
+    async function getPackageUpdates(packageConfigPromise) {
+        const packageConfig = await packageConfigPromise;
+        const updates = await getUpdates(packageConfig.manifest);
+        return Object.assign({}, packageConfig, {
+            updates,
+        });
+    }
+
+    async function createPRBranches(packageConfigPromise) {
+        const packageConfig = await packageConfigPromise;
+        if (!packageConfig.prExists && packageConfig.updates.length) {
+            const headBranch = getBranchName(packageConfig.path);
+            const { sha: branchHead } = await client.repos.getShaOfCommitRef({
+                user,
+                repo,
+                ref: packageConfig.baseBranch,
+            });
+            await client.gitdata.createReference({
+                user,
+                repo,
+                sha: branchHead,
+                ref: `refs/heads/${headBranch}`,
+            });
+            return Object.assign({}, packageConfig, { headBranch });
+        }
+        return packageConfig;
+    }
+
+    async function createUpdateCommits(packageConfigPromise) {
+        const packageConfig = await packageConfigPromise;
+        for (let update of packageConfig.updates) {
+            const newManifest = Object.assign({}, packageConfig.manifest);
+            newManifest[update.type][update.name] = update.latest;
+
+            const dir = dirname(packageConfig.manifestPath);
+            const { tree } = await client.gitdata.getTree({
+                user,
+                repo,
+                sha: `${packageConfig.headBranch}:${dir}`,
+            });
+            const item = tree.find(matchManifest);
+
+            await client.repos.updateFile({
+                user,
+                repo,
+                path: packageConfig.manifestPath,
+                message: `chore: update "${update.name}" in "${update.type}"`,
+                content: new Buffer(
+                    JSON.stringify(newManifest, null, 2),
+                ).toString('base64'),
+                sha: item.sha,
+                branch: packageConfig.headBranch,
+            });
+
+            if (packageConfig.lockFiles.length) {
+                // TODO: update lock files
+            }
+        }
+        return packageConfig;
+    }
+
+    async function createPullRequest(packageConfigPromise) {
+        const packageConfig = await packageConfigPromise;
+        if (!packageConfig.prExists) {
+            await client.pullRequests.create({
+                user,
+                repo,
+                title: `Update dependencies for ${packageConfig.path}`,
+                body: unindent(`
+Bazinga!
+
+This PR has been automatically created through the [dependency-updater](https://github.com/nerdlabs/dependency-updater).
+
+It contains updates for all your \`dependencies\` and \`devDependencies\` in
+the following package: \`${packageConfig.path}\`.
+                `),
+                head: packageConfig.headBranch,
+                base: packageConfig.baseBranch,
+            });
+        }
+        return packageConfig;
+    }
+
+    return await Promise.all(
+        manifests
+            .map(getPackageConfig)
+            .map(setBranchConfig)
+            .map(parsePackageFiles)
+            .map(getPackageUpdates)
+            .map(createPRBranches)
+            .map(createUpdateCommits)
+            .map(createPullRequest),
+    );
 }
